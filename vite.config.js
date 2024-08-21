30270a0a5de7ec438046c88907815e0e731375aa
LashmanSingh <lashmansingh@academic.rrc.ca>
2024-08-21 10:50:14 -0500

    Updated player import statement for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tic-tac-toe-game/"
+  base: '/tic-tac-toe-game/', 
+  build: {
+    outDir: 'dist',
+  },
 })
